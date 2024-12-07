3a4b5f32821db4ed71ab8ddae7858fa28b37acf3
lujiangz <31911561+lujiangz@users.noreply.github.com>
2024-12-07 17:09:20 +0300

    Delete tools/js/formyoo.php
    wrking good

--- tools/js/formyoo.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pastebin_url = "https://pastebin.com/raw/URxqMTFh";
-$sözler = file($pastebin_url, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-if ($sözler) {
-    echo $sözler[array_rand($sözler)];
-} else {
-    echo "Bugün söz bulamadım! 😅";
-}
-?>
